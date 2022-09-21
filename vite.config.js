08e4fb5411e499c1d95b79892264aea2e371551d
mqwebst2 <marques.q.webster@gmail.com>
2022-09-21 11:21:46 -0500

    multi page build config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig, resolveBaseUrl } from 'vite';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
