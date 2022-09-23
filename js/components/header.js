cc4523ef44b449ea2eaf1cd245d3e6262a6d7bce
mqwebst2 <marques.q.webster@gmail.com>
2022-09-22 20:19:31 -0500

    html and css updates

---
+++ js/components/header.js
@@ -0,0 +1,31 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <div id="nav-bar">
+        <div id="nav-bar__con">
+          <div id="nav-bar__logo">
+            <img src="./mw-logo.png" alt="Marques Webster" id="logo" />
+          </div>
+
+          <div id="nav-bar__menu">
+            <div id="nav-bar__menu-items">
+              <ul class="menu-items__list">
+                <li class="menu-items__list-item"><a href="#">Home</a></li>
+                <li class="menu-items__list-item"><a href="#">About</a></li>
+                <li class="menu-items__list-item"><a href="#">Resume</a></li>
+                <li class="menu-items__list-item"><a href="#">Portfolio</a></li>
+                <li class="menu-items__list-item"><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
