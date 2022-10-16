896566a4cd03c0182d9c733007d1a54b4d7276b9
mqwebst2 <marques.q.webster@gmail.com>
2022-10-16 04:12:39 -0500

    commented out service worker

--- vite.config.js
+++ vite.config.js
@@ -21,41 +21,41 @@ export default defineConfig({
     },
   },
 
-  plugins: [
-    VitePWA({
-      strategies: 'injectManifest',
-      filename: './sw.js',
-      devOptions: {
-        enabled: true,
-        type: 'module',
-      },
-      injectManifest: { globPatterns: ['**/*.html'] },
-      manifest: {
-        name: 'TEP Project',
-        short_name: 'TEP Project',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#eee',
-        description:
-          'Custom comment app with internal database (IndexedDB) and PWA functionality with service worker',
-        icons: [
-          {
-            src: './icon_48.png',
-            sizes: '48x48',
-            type: 'image/png',
-          },
-          {
-            src: './icon_48.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: './icon_48.png',
-            sizes: '72x72',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
+  // plugins: [
+  //   VitePWA({
+  //     strategies: 'injectManifest',
+  //     filename: './sw.js',
+  //     devOptions: {
+  //       enabled: true,
+  //       type: 'module',
+  //     },
+  //     injectManifest: { globPatterns: ['**/*.html'] },
+  //     manifest: {
+  //       name: 'TEP Project',
+  //       short_name: 'TEP Project',
+  //       start_url: '/',
+  //       display: 'standalone',
+  //       background_color: '#eee',
+  //       description:
+  //         'Custom comment app with internal database (IndexedDB) and PWA functionality with service worker',
+  //       icons: [
+  //         {
+  //           src: './icon_48.png',
+  //           sizes: '48x48',
+  //           type: 'image/png',
+  //         },
+  //         {
+  //           src: './icon_48.png',
+  //           sizes: '64x64',
+  //           type: 'image/png',
+  //         },
+  //         {
+  //           src: './icon_48.png',
+  //           sizes: '72x72',
+  //           type: 'image/png',
+  //         },
+  //       ],
+  //     },
+  //   }),
+  // ],
 });
